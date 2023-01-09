dcdf5862fc079c13377fba23c6d9f0b9a20afb8e
syulyul <syul9800@gmail.com>
2023-01-09 13:55:04 +0900

    create project

---
+++ src/main/java/org/zerock/w2/domain/TodoVO.java
@@ -0,0 +1,26 @@
+package org.zerock.w2.domain;
+
+//import lombok.Builder;
+//import lombok.Getter;
+//import lombok.ToString;
+
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Builder
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class TodoVO {
+
+    private Long tno;
+
+    private String title;
+
+    private LocalDate dueDate;
+
+    private boolean finished;
+
+}
